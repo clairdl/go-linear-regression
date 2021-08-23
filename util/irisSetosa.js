ba50377faacd23579c1bde8807f1bdd936a46236
clairdl <52753311+clairdl@users.noreply.github.com>
2021-08-23 00:38:58 -0700

    addes utils for iris dataset

---
+++ util/irisSetosa.js
@@ -0,0 +1,29 @@
+const csv = require('csv-parse');
+const createCsvWriter = require('csv-writer').createArrayCsvWriter;
+
+const fs = require('fs');
+const outPath = '../iris-setosa.csv';
+
+let res = [];
+
+fs.createReadStream('../iris_headers.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+    if (row.pop() === 'Iris-setosa') {
+      res.push(row);
+      console.log(row);
+    }
+  })
+  .on('error', (err) => console.log('Error: ', err))
+  .on('end', () => {
+    csvWriter.writeRecords(res)
+      .then(() => {
+        console.log('Successfully written to:', outPath);
+      });
+});
+
+const csvWriter = createCsvWriter({
+  header: ['Sepal length', 'Sepal width', 'Petal length', 'Petal width'],
+  path: outPath,
+});
+
